b621110f2051c65ae1387a7cdb36c69b4daea5dc
kushagra <agrakush4@gmail.com>
2025-04-07 14:31:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let containers = document.querySelectorAll('.project-item');
+let timeout;
+
+containers.forEach(container => {   
+  let box = container.querySelectorAll('p');
+  
+  container.addEventListener('mouseenter', () => {
+    timeout = setTimeout(() => {
+      box.forEach(p => {
+        p.style.display = 'block';
+      });
+    }, 1122);
+  });
+
+  container.addEventListener('mouseleave', () => {
+    clearTimeout(timeout);
+    box.forEach(p => {
+      p.style.display = 'none';
+    });
+  });
+});
